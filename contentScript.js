2c603786902dc8b7e8111f8d84a0e61034f751e3
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-01-16 15:17:35 -0500

    replace with implementation

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,7 @@
-const FIND = "replace me with regex expression, don't forget the slashes instead of quotations";
+const FIND = /(\d.?\d+|\d{1,3}(,\d{3})(.\d+)?)\s?([yzafpnumcdkKMGTPEZY]?m)/;
 const REPLACE_WITH = "UNIT FOUND AND REPLACED";
 const regex = new RegExp(FIND,"gi");
 const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
 for(const snippet of contents){
-    snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
+    snippet.textContent = snippet.textContent.replace(regex,REPLACE_WITH);
 }
