c2171591a28fc562ee7e05c94fb7f135f7f14033
Nikko Ong <nikkoong98@gmail.com>
2021-01-16 15:50:45 -0500

    Update contentScript.js
    updated regex

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-const FIND = /(\d.?\d+|\d{1,3}(,\d{3})(.\d+)?)\s?([yzafpnumcdkKMGTPEZY]?m)/;
+const FIND = /(\d*\.?\d+|\d{1,3}(,\d{3})*(\.\d+)?)\s?(exa|peta|tera|giga|mega|kilo|hecto|deca|deci|centi|mili|micro|nano|pico|femto|atto|zepto|yotta|[yzafpnumcdhkKMGTPEZY])?(miles|mi|(in(ch)|feet|ft|A|yard|yd|fathom|furlong|rod|league|chain|m(eters)?))((e)?s)?/;
 const REPLACE_WITH = "UNIT FOUND AND REPLACED";
 const regex = new RegExp(FIND,"gi");
 const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
