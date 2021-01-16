40361bc9e7eb021cbfe95f4ed4fcc940e7d80401
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-01-16 16:26:15 -0500

    right click context menu

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
-// Background.js
+chrome.runtime.onInstalled.addListener(function() {
+    var context = "selection";
+    var title = "Smoot it!";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context}); 
+});
+
+function onClickHandler(info, tab) {
+    var sText = info.selectionText;
+    var url = "https://www.google.com/search?q=" + encodeURIComponent(sText); 
+    window.open(url, '_blank');
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
