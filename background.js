3b0329d4cf1be2f1861b467e70a902ec17b52bf1
Nikko Ong <nikkoong98@gmail.com>
2021-01-16 17:03:09 -0500

    Update background.js
    updated regex and return values

--- background.js
+++ background.js
@@ -6,8 +6,13 @@ chrome.runtime.onInstalled.addListener(function() {
 
 function onClickHandler(info, tab) {
     var sText = info.selectionText;
-    var url = "https://www.google.com/search?q=" + encodeURIComponent(sText); 
-    window.open(url, '_blank');
+    var re = /(\d*\.?\d+|\d{1,3}(,\d{3})*(\.\d+)?)\s?(exa|peta|tera|giga|mega|kilo|hecto|deca|deci|centi|mili|micro|nano|pico|femto|atto|zepto|yotta|[yzafpnumcdhkKMGTPEZY])?(miles|mi|(in(ch)|feet|ft|A|yard|yd|fathom|furlong|rod|league|chain|altuve|parsec|beard\s?-?second|smoot|light\s?-?year|m(eters)?))e?s?/i;
+    var info = sText.match(re);
+    console.log(info);
+    var myNum = info[0];
+    var prefix = info[3];
+    var unit = info[4];
+    return myNum, prefix, unit;
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
