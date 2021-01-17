85d8f9a2ddea7030be3a92d25f967f8084b55119
Nikko Ong <nikkoong98@gmail.com>
2021-01-16 20:45:26 -0500

    Update background.js
    updated regex and added support for "thousand" "million"

--- background.js
+++ background.js
@@ -6,14 +6,14 @@ chrome.runtime.onInstalled.addListener(function() {
 
 function onClickHandler(info, tab) {
     var sText = info.selectionText;
-    var re = /(\d*\.?\d+|\d{1,3}(,\d{3})*(\.\d+)?)\s?(exa|peta|tera|giga|mega|kilo|hecto|deca|deci|centi|mili|micro|nano|pico|femto|atto|zepto|yotta|[yzafpnumcdhkKMGTPEZY])?(miles|mi|(in(ch)|feet|ft|A|yard|yd|fathom|furlong|rod|league|chain|altuve|parsec|beard\s?-?second|smoot|light\s?-?year|m(eters)?))e?s?/i;
+    var re = /(\d*\.?\d+|\d{1,3}(,\d{3})*(\.\d+)?)\s?(hundred|thousand|million|billion|trillion|quadrillion|quintillion|sextillion)?\s?(exa|peta|tera|giga|mega|kilo|hecto|deca|deci|centi|mili|micro|nano|pico|femto|atto|zepto|yotta|[yzafpnumcdhkKMGTPEZY])?(miles|mi|(in(ch)|feet|ft|A|yard|yd|fathom|furlong|rod|league|chain|altuve|parsec|beard\s?-?second|smoot|light\s?-?year|m(eters)?))e?s?/gi;
     var info = sText.match(re);
-    console.log(info);
     var myNum = info[1];
-    var prefix = info[4];
-    var unit = info[5];
-    alert('The number is ' + myNum + '\nThe prefix is ' + prefix + '\nThe unit is ' +unit);
-    return myNum, prefix, unit;
+    var quantity = info[4]
+    var prefix = info[5];
+    var unit = info[6];
+    alert('The number is ' + myNum + '\nThe quantity is ' + quantity + '\nThe prefix is ' + prefix + '\nThe unit is ' +unit);
+    return myNum, quantity, prefix, unit;
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
