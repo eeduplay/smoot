3c065a8bf2be041a1f0176f88c3a0b0d8ead5aed
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-01-16 20:53:06 -0500

    backend request

--- background.js
+++ background.js
@@ -6,14 +6,26 @@ chrome.runtime.onInstalled.addListener(function() {
 
 function onClickHandler(info, tab) {
     var sText = info.selectionText;
-    var re = /(\d*\.?\d+|\d{1,3}(,\d{3})*(\.\d+)?)\s?(hundred|thousand|million|billion|trillion|quadrillion|quintillion|sextillion)?\s?(exa|peta|tera|giga|mega|kilo|hecto|deca|deci|centi|mili|micro|nano|pico|femto|atto|zepto|yotta|[yzafpnumcdhkKMGTPEZY])?(miles|mi|(in(ch)|feet|ft|A|yard|yd|fathom|furlong|rod|league|chain|altuve|parsec|beard\s?-?second|smoot|light\s?-?year|m(eters)?))e?s?/i;
-    var info = sText.match(re);
-    var myNum = info[1];
-    var quantity = info[4]
-    var prefix = info[5];
-    var unit = info[6];
-    alert('The number is ' + myNum + '\nThe quantity is ' + quantity + '\nThe prefix is ' + prefix + '\nThe unit is ' +unit);
-    return myNum, quantity, prefix, unit;
+    var re = /(\d*\.?\d+|\d{1,3}(,\d{3})*(\.\d+)?)\s?(exa|peta|tera|giga|mega|kilo|hecto|deca|deci|centi|mili|micro|nano|pico|femto|atto|zepto|yotta|[yzafpnumcdhkKMGTPEZY])?(miles|mi|(in(ch)|feet|ft|A|yard|yd|fathom|furlong|rod|league|chain|altuve|parsec|beard\s?-?second|smoot|light\s?-?year|m(eters)?))e?s?/i;
+    var parsedExpression = sText.match(re);
+    var value = parsedExpression[1];
+    var prefix = parsedExpression[4];
+    var unit = parsedExpression[5];
+    
+    let requestURL = 'https://smootthenorth.ue.r.appspot.com/query?unit='+ prefix + unit + '&value=' + value;
+    let request = new XMLHttpRequest();
+    request.open('GET',requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        const bounds = request.response;
+        var topBoundValue = bounds["tvalue"];
+        var botBoundValue = bounds["bvalue"];
+        var topName = bounds["tname"];
+        var botName = bounds["bvalue"];
+
+        alert("Top Bound: " + topBoundValue + topName + "\nBottom Bound: " + botBoundValue + botName);
+    }
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
