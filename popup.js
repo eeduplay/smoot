e5da9ae6d5ad17ad7d079a5001634b6b8076e3b6
eeduplay <gherrerarausaure@gmail.com>
2021-01-17 15:39:36 +0100

    display response in UI

--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.msg === "values") {
+            var uval = request.msg.data.top_value;
+            var lval = request.msg.data.bot_value;
+            var tickwidth = 300*(request.data.query_value-lval)/(uval-lval);
             document.getElementById('input').innerHTML = request.data.query_value + " m";
+            document.getElementById('maintick').setAttribute('style', "width: "+tickwidth+"px");
+            document.getElementById('lowerName').innerHTML = request.msg.data.bot_units;
+            document.getElementById('upperName').innerHTML = request.msg.data.top_units;
         }
     }
 );
